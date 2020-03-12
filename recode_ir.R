f20e1d22cd10a0f14e44e07b11aa0c66217205bd
Lea Vanheyden <elektronische_post@gmx.de>
2020-03-12 13:34:26 +0100

    data.frames erstellen

--- recode_ir.R
+++ recode_ir.R
@@ -1,4 +1,28 @@
-# neuen data.frame initialisieren
+## Data.frame LVS = TRUE erstellen
 
-new_data <- 
+# enthält alle Messungen von Personen mit LVS-Gerät und soll nachher mit 
+# anderem data.frame (Personen ohne Gerät) zusammengeführt werden
+
+lvs_true_data <- data %>%
+                  # nur die Beacon (also LVS) Fälle
+                  subset(type == "Beacon") %>%
+                  # unnötige Spalten löschen
+                  select(date, time) %>%
+                  # neue Spalte hinzufügen
+                  mutate(lvs = TRUE)
+
+## Data.frame LVS = FALSE erstellen
+
+# enthält anfangs alle Infrarotmessungen
+
+lvs_false_data <- data %>%
+                    # nur die Infrarot Fälle
+                  subset(type == "Infrared") %>%
+                  # unnötige Spalten löschen
+                  select(date, time) %>%
+                  # neue Spalte hinzufügen
+                  mutate(lvs = FALSE)
+
+# Falls eine Beaconmessung zugeordnet werden kann, soll die Infrarotmessung
+# entfernt werden
 

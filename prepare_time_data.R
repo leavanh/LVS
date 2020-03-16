2b64f7cad4dcbf1934408e9ea2eef306b18c4f22
Lea Vanheyden <lea.schulz-vanheyden@gmx.de>
2020-03-16 21:41:33 +0100

    replace_many fehlt noch

--- prepare_time_data.R
+++ prepare_time_data.R
@@ -10,5 +10,34 @@ library("tidyverse")
 
 ## Daten laden
 
-data <- readRDS(file = "Daten/data.RDS")
-date_data <- readRDS(file = "Daten/date_data.RDS")
+lvs_data <- readRDS(file = "Daten/lvs_data.RDS")
+lvs_date_data <- readRDS(file = "Daten/lvs_date_data.RDS")
+
+## neuer Datensatz
+
+time_data <- lvs_data
+
+## Schleife
+
+# Zeit von 0 ohne 5 soll umkodiert werden
+
+time_interval <- interval(
+  as.POSIXct("1899-12-31 00:00:00", tz = "UTC"),
+  as.POSIXct("1899-12-31 04:59:59", tz = "UTC")
+)
+
+for(i in 1:nrow(time_data)) {
+  # Zeit der Beobachtung 
+  time_i <- time_data[[i, "time"]]
+  # Datum der Beobachtung
+  date_i <- time_data[[i, "date"]]
+  # prüfen, ob Zeit von 0 bis 5 Uhr ist und umkodiert werden muss
+  if(time_i %within% time_interval) {
+    # Datum des Tags davor
+    date_new <- date_i - days(1)
+    # neue Werte zuweisen
+    time_data <- replaye_many()
+  }
+  # mit der nächsten Messung weitermachen
+  i <- i + 1
+}

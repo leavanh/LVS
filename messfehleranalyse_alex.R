a6f96cf6c0d047d26eaa7cdd98e216d4e0372ce6
Alexander Fogus <alex.fogus@gmail.com>
2020-05-07 14:34:07 +0200

    Messfehleranalyse zusammengefasst

--- messfehleranalyse_alex.R
+++ messfehleranalyse_alex.R
@@ -1,4 +1,145 @@
 
+# Von den Studenten gezählte Personen, sortiert nach Erfassungsart am 27.02
+sums_27$type2 <- factor(sums_27$type, as.character(sums_27$type)) 
+ggplot(sums_27, aes(x = type2, y = sum)) +
+  geom_bar(aes(x=type2), data=sums_27, stat="identity") +
+  labs(title = "Personen am 27.02.2019 (Manuelle Messung)",
+       x = "Personen",
+       y = "absolute Häufigkeit") +
+  scale_x_discrete(labels=c("aK_gesamt" = "Erfasste und \n nicht erfasste \n andere Kontakte", 
+                            "erfasst" = "Erfasst \n andere Kontakte und \n Skitourengeher",
+                            "Erfasst_aK" = "Erfasste \n andere Kontakte", "Erfasst_SG"= "Erfasste \n Skitourengeher", 
+                            "gesamt" = "Erfasste und \n nicht erfasste Skitourengeher \n und andere Kontakte", 
+                            "nicht_erfasst" = "Nicht erfasste \n Skitourengeher \n andere Kontakte",
+                            "Nicht_erfasst_aK" = "Nicht erfasste \n andere Kontakte", 
+                            "Nicht_erfasst_SG" = "Nicht erfasste \n Skitourengeher", 
+                            "SG_gesamt" = "Erfasste und \n nicht erfasste \n Skitourengeher")) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black")) 
+
+# Von den Studenten gezählte Personen, sortiert nach Erfassungsart am 27.02, ohne Berechnungen
+plot_zlg27 <-ggplot(sums_27, aes(x = type2, y = sum)) +
+  geom_bar(aes(x=type2), data=sums_27, stat="identity") +
+  labs(title = "Personen am 27.02.2019 (Manuelle Messung)",
+       x = "Personen",
+       y = "absolute Häufigkeit") +
+  scale_x_discrete(labels=c("aK_gesamt" = "Erfasste und \n nicht erfasste \n andere Kontakte", 
+                            "erfasst" = "Erfasst \n andere Kontakte und \n Skitourengeher",
+                            "Erfasst_aK" = "Erfasste \n andere Kontakte", "Erfasst_SG"= "Erfasste \n Skitourengeher", 
+                            "gesamt" = "Erfasste und \n nicht erfasste Skitourengeher \n und andere Kontakte", 
+                            "nicht_erfasst" = "Nicht erfasste \n Skitourengeher \n andere Kontakte",
+                            "Nicht_erfasst_aK" = "Nicht erfasste \n andere Kontakte", 
+                            "Nicht_erfasst_SG" = "Nicht erfasste \n Skitourengeher", 
+                            "SG_gesamt" = "Erfasste und \n nicht erfasste \n Skitourengeher"),
+                   limit = c( "Erfasst_SG", "Nicht_erfasst_SG", "Erfasst_aK", "Nicht_erfasst_aK")) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
+plot_zlg27
+
+# Von den Studenten gezählte Personen, sortiert nach Erfassungsart am 28.02
+sums_28$type2 <- factor(sums_28$type, as.character(sums_28$type)) 
+ggplot(sums_28, aes(x = type2, y = sum)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Anzahl nach Art der Erfassungen am 28.02.",
+       x = "Art der Erfassung",
+       y = "absolute Anzahl") +
+  scale_x_discrete(labels=c("aK_gesamt" = "Erfasste und \n nicht erfasste \n andere Kontakte", 
+                            "erfasst" = "Erfasst \n andere Kontakte und \n Skitourengeher",
+                            "Erfasst_aK" = "Erfasste \n andere Kontakte", "Erfasst_SG"= "Erfasste \n Skitourengeher", 
+                            "gesamt" = "Erfasste und \n nicht erfasste Skitourengeher \n und andere Kontakte", 
+                            "nicht_erfasst" = "Nicht erfasste \n Skitourengeher \n andere Kontakte",
+                            "Nicht_erfasst_aK" = "Nicht erfasste \n andere Kontakte", 
+                            "Nicht_erfasst_SG" = "Nicht erfasste \n Skitourengeher", 
+                            "SG_gesamt" = "Erfasste und \n nicht erfasste \n Skitourengeher")) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
+
+# Von den Studenten gezählte Personen, sortiert nach Erfassungsart am 27.02, ohne Berechnungen
+plot_zlg28 <- ggplot(sums_28, aes(x = type2, y = sum)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Anzahl nach Art der Erfassungen am 28.02.",
+       x = "Art der Erfassung",
+       y = "absolute Anzahl") +
+  scale_x_discrete(labels=c("aK_gesamt" = "Erfasste und \n nicht erfasste \n andere Kontakte", 
+                            "erfasst" = "Erfasst \n andere Kontakte und \n Skitourengeher",
+                            "Erfasst_aK" = "Erfasste \n andere Kontakte", "Erfasst_SG"= "Erfasste \n Skitourengeher", 
+                            "gesamt" = "Erfasste und \n nicht erfasste Skitourengeher \n und andere Kontakte", 
+                            "nicht_erfasst" = "Nicht erfasste \n Skitourengeher \n andere Kontakte",
+                            "Nicht_erfasst_aK" = "Nicht erfasste \n andere Kontakte", 
+                            "Nicht_erfasst_SG" = "Nicht erfasste \n Skitourengeher", 
+                            "SG_gesamt" = "Erfasste und \n nicht erfasste \n Skitourengeher"),
+                   limit = c( "Erfasst_SG", "Nicht_erfasst_SG", "Erfasst_aK", "Nicht_erfasst_aK")) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
+plot_zlg28
+
+
+
+
+
+
+
+
+
+
+
+## Studentenmessungen vs Checkpointmessungen
+
+ggplot(data = filter(zlg_beide_bereinigt_summen, type %in% c("checkpoint", "gesamt"))) +
+  geom_bar(aes(x = type, y = sum), stat = "identity") +
+  labs(title = "Vergleich der Checkpoint- und Studentendaten",
+       x = "Art der Messung", y = "Anzahl")
+
+# als Anteil in Prozent
+
+zlg_beide_bereinigt_summen$sum[zlg_beide_bereinigt_summen$type == "checkpoint"] / 
+  zlg_beide_bereinigt_summen$sum[zlg_beide_bereinigt_summen$type == "gesamt"]
+
+
+
+# als Anteil in Prozent
+
+zlg_beide_bereinigt_summen$sum[zlg_beide_bereinigt_summen$type == "aK_gesamt"] / 
+  zlg_beide_bereinigt_summen$sum[zlg_beide_bereinigt_summen$type == "gesamt"]
+
+
+
+
+
+
+
+
+## Summen als Plot nebeneinander
+
+zlg_beide_summen <- 
+  mutate(zlg_beide_summen, 
+         erfassung = as.factor(c("erfasst", "nicht_erfasst", "erfasst", 
+                                 "nicht_erfasst", "erfasst", "nicht_erfasst", 
+                                 "beide", "beide", "beide", "beide")),
+         kontakt = c("SG", "SG", "aK", "aK", "beide", "beide", "SG", "aK",
+                     "beide", "beide"))
+
+
+
+# Erfassungen vs Nichterfassungen zu allen Daten
+
+ggplot(data = filter(zlg_beide_summen, type %in% c("nicht_erfasst", "erfasst"))) +
+  geom_bar(aes(x = type, y = sum), stat = "identity") +
+  labs(title = "Vergleich der Erfassungen und Nichterfassungen",
+       x = "Art der Messung", y = "Anzahl")
+
+# Anteil Nichterfassungen in Prozent
+
+zlg_beide_summen$sum[zlg_beide_summen$type == "nicht_erfasst"] / 
+  zlg_beide_summen$sum[zlg_beide_summen$type == "gesamt"]
+
+
+
+# Unterscheidung zwischen Kontakten und Erfassung
+
 ggplot(zlg_beide_summen[1:4,], 
        aes(x = factor(kontakt, levels = c("SG", "aK")),
            y = sum, fill = erfassung)) +
@@ -17,43 +158,6 @@ ggplot(zlg_beide_summen[1:4,],
                     values = c("cadetblue", "antiquewhite4")) +
   geom_text(aes(label=sum), position=position_dodge(width=0.9), vjust=-0.25)
 
-# Studentische zählung, erfassung und nicht erfassung von Gruppen
-ggplot(data = erfassung_je_gruppe, 
-       aes(x = grösse, y = anzahl, fill = type)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  labs(title = "Manuelle Zählung am 27.02- und 28.02.2019 \n nach Gruppengröße",
-       x = "Gruppengröße",
-       y = "Anzahl") +
-  scale_x_continuous(breaks = 1:8) +
-  theme(
-    axis.line = element_line(colour = "black"),
-    text = element_text(size= 15)) +
-  #scale_fill_discrete(name = NULL, labels = c("erfasst", "nicht erfasst"))
-  scale_fill_manual(name = NULL, labels = c("Erfasst", "Nicht Erfasst"), 
-                    values = c("cadetblue", "antiquewhite4")) +
-  theme(axis.line = element_line(colour = "black"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        panel.background = element_blank()) 
-
-#Checkpointerfassungen am 27-02 und 28-02-2019
-zlg_ckpt_beide_bereinigt %>%
-  ggplot(aes(date)) +
-  geom_col(aes(y = erfasst, fill = "cadetblue")) +
-  #geom_col(aes(y = lvs_true, fill = "blue")) +
-  scale_fill_identity(name = "Messung",
-                      breaks = c("cadetblue"),
-                      labels = c("Erfasst"),
-                      guide = "legend") +
-  labs(title = "Checkpoint Zählung am 27.02- und 28.02.2019",
-       x = "Datum",
-       y = "Absolute Häufigkeit") +
-  theme(axis.line = element_line(colour = "black"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        panel.background = element_blank())
 
 ### Erfassungen in 3 min intervall
 erf_zeit_27_plot <- 
@@ -120,6 +224,27 @@ erf_zeit_28_02_plot <-
 erf_zeit_grid <- 
   grid.arrange(erf_zeit_27_plot, erf_zeit_28_01_plot, erf_zeit_28_02_plot,
                nrow = 3)
+
+# Studentische zählung, erfassung und nicht erfassung von Gruppen
+ggplot(data = erfassung_je_gruppe, 
+       aes(x = grösse, y = anzahl, fill = type)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Manuelle Zählung am 27.02- und 28.02.2019 \n nach Gruppengröße",
+       x = "Gruppengröße",
+       y = "Anzahl") +
+  scale_x_continuous(breaks = 1:8) +
+  theme(
+    axis.line = element_line(colour = "black"),
+    text = element_text(size= 15)) +
+  #scale_fill_discrete(name = NULL, labels = c("erfasst", "nicht erfasst"))
+  scale_fill_manual(name = NULL, labels = c("Erfasst", "Nicht Erfasst"), 
+                    values = c("cadetblue", "antiquewhite4")) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
 
 #Verhältnis der Gruppengröße
 ggplot(data = erfassung_je_gruppe) +
--- prepare_data_1920.R
+++ prepare_data_1920.R
@@ -4,13 +4,16 @@
 
 library(readxl)
 library(tidyverse)
+library(gridExtra)
 
 # dazu aus den beiden .txt-Files rauskopiert und in Exceldatei eingefügt
 
 # welcher war Nord, welcher Süd?
 
-data_1920_01 <- read_excel("Daten/Daten_1920.xlsx", sheet = "1920_01", col_names = c("type", "date", "time"))
-data_1920_02 <- read_excel("Daten/Daten_1920.xlsx", sheet = "1920_02", col_names = c("type", "date", "time"))
+data_1920_01 <- read_excel("Daten/Daten_1920.xlsx", sheet = "1920_01", 
+                           col_names = c("type", "date", "time"))
+data_1920_02 <- read_excel("Daten/Daten_1920.xlsx", sheet = "1920_02", 
+                           col_names = c("type", "date", "time"))
 
 data_1920 <- rbind(data_1920_01, data_1920_02)
 
@@ -21,32 +24,52 @@ all_checkpoint_stats_1920 <- subset(data_1920,
 
 # Zeit und Datum in passendes Foramt umwandeln
 
-all_checkpoint_stats_1920$date <- as.POSIXct(all_checkpoint_stats_1920$date, format = "%d %B %Y", tz = "UTC")
-all_checkpoint_stats_1920$time <- as.POSIXct(all_checkpoint_stats_1920$time, format = "%d %B %Y", tz = "UTC")
+all_checkpoint_stats_1920$date <- as.POSIXct(all_checkpoint_stats_1920$date, 
+                                             format = "%d %B %Y", tz = "UTC")
+all_checkpoint_stats_1920$time <- as.POSIXct(all_checkpoint_stats_1920$time, 
+                                             format = "%d %B %Y", tz = "UTC")
 
 
 # manuelle Zählung einlesen
 
-zaehlung_lvs_check_27_02 <- read_excel("Daten/Zaehlung_LVS-check.xlsx", sheet = 1, skip = 1,
-                                       col_names = c("time", "Erfasst_SG", "Nicht_erfasst_SG", "Erfasst_aK", "Nicht_erfasst_aK"))
-zaehlung_lvs_check_28_02 <- read_excel("Daten/Zaehlung_LVS-check.xlsx", sheet = 2, skip = 1,
-                                       col_names = c("time", "Erfasst_SG", "Nicht_erfasst_SG", "Erfasst_aK", "Nicht_erfasst_aK"))
+zaehlung_lvs_check_27_02 <- read_excel("Daten/Zaehlung_LVS-check.xlsx", 
+                                       sheet = 1, skip = 1,
+                                       col_names = c("time", "Erfasst_SG", 
+                                                     "Nicht_erfasst_SG", 
+                                                     "Erfasst_aK", 
+                                                     "Nicht_erfasst_aK"))
+zaehlung_lvs_check_28_02 <- read_excel("Daten/Zaehlung_LVS-check.xlsx", 
+                                       sheet = 2, skip = 1,
+                                       col_names = c("time", "Erfasst_SG", 
+                                                     "Nicht_erfasst_SG", 
+                                                     "Erfasst_aK", 
+                                                     "Nicht_erfasst_aK"))
 
 # Checkpoint-Daten an den Tagen der manuellen Zählung
 
-checkpoint_stats_27_02 <- filter(all_checkpoint_stats_1920, date == as.POSIXct("2020-02-27", tz = "UTC"))
-checkpoint_stats_28_02 <- filter(all_checkpoint_stats_1920, date == as.POSIXct("2020-02-28", tz = "UTC"))
+checkpoint_stats_27_02 <- filter(all_checkpoint_stats_1920, 
+                                 date == as.POSIXct("2020-02-27", tz = "UTC"))
+checkpoint_stats_28_02 <- filter(all_checkpoint_stats_1920, 
+                                 date == as.POSIXct("2020-02-28", tz = "UTC"))
 
 
 ## Checkpointmessungen im Zeitraum der manuellen Messungen filtern
 
-ckpt_27 <- subset(checkpoint_stats_27_02, time >= as.POSIXct('1899-12-31 10:30', tz = "UTC") &
+ckpt_27 <- subset(checkpoint_stats_27_02, 
+                  time >= as.POSIXct('1899-12-31 10:30', tz = "UTC") &
                     time <= as.POSIXct('1899-12-31 12:58', tz = "UTC"))
 
-ckpt_28 <- rbind(subset(checkpoint_stats_28_02, time >= as.POSIXct('1899-12-31 10:38', tz = "UTC") &
+ckpt_28 <- rbind(subset(checkpoint_stats_28_02, 
+                        time >= as.POSIXct('1899-12-31 10:38', tz = "UTC") &
                           time <= as.POSIXct('1899-12-31 12:27', tz = "UTC")),
-                 subset(checkpoint_stats_28_02, time >= as.POSIXct('1899-12-31 14:39', tz = "UTC") &
+                 subset(checkpoint_stats_28_02, 
+                        time >= as.POSIXct('1899-12-31 14:39', tz = "UTC") &
                           time <= as.POSIXct('1899-12-31 16:51', tz = "UTC")))
+
+ckpt_beide <- rbind(ckpt_27, ckpt_28)
+
+
+
 
 
 # Checkpoint-Stats nach type
@@ -61,8 +84,10 @@ zaehlung_lvs_check_28_02[is.na(zaehlung_lvs_check_28_02)] <- 0
 
 # Zählungen zu numerischen Vektoren machen
 
-zaehlung_lvs_check_27_02[, 2:5] <- as.numeric(unlist(zaehlung_lvs_check_27_02[, 2:5]))
-zaehlung_lvs_check_28_02[, 2:5] <- as.numeric(unlist(zaehlung_lvs_check_28_02[, 2:5]))
+zaehlung_lvs_check_27_02[, 2:5] <- 
+  as.numeric(unlist(zaehlung_lvs_check_27_02[, 2:5]))
+zaehlung_lvs_check_28_02[, 2:5] <- 
+  as.numeric(unlist(zaehlung_lvs_check_28_02[, 2:5]))
 
 # restliche NAs entfernen
 
@@ -103,4 +128,191 @@ zlg_ckpt_28 <- ckpt_28 %>% group_by(time) %>%
 
 zlg_ckpt_beide <- rbind(mutate(zlg_ckpt_27, date = "27.02."), 
                    mutate(zlg_ckpt_28, date = "28.02."))
+
+
+# Zeitlimit der manuellen Messungen für die Plots später
+
+time_limits <- as.POSIXct(strptime(c("1899-12-31 10:00:00","1899-12-31 17:00:00"), 
+                                   format = "%Y-%m-%d %H:%M", tz = "UTC"))
+
+
+
+## Summen der einzelnen Reihen in der manuellen Zählung: 
+# SG, aK, erfasst, nicht erfasst
+
+(summen_27 <- apply(zlg_27[,-1], 2, sum))
+(summen_28 <- apply(zlg_28[,-1], 2, sum))
+
+# Summen als separate Tabelle
+
+sums_27 <- data.frame(cbind(names(summen_27), as.numeric(summen_27)))
+names(sums_27) <- c("type", "sum")
+sums_27$sum <- as.numeric(as.character(sums_27$sum))
+
+sums_28 <- data.frame(cbind(names(summen_28), as.numeric(summen_28)))
+names(sums_28) <- c("type", "sum")
+sums_28$sum <- as.numeric(as.character(sums_28$sum))
+
+
+
+# gruppiert in 3-Minuten-Intervalle
+
+zlg_27_breaks_3min <- mutate(zlg_27, breaks = cut(zlg_27$time, 
+                                                  breaks = "3 min"))
+zlg_27_grouped_3min <- zlg_27_breaks_3min %>% group_by(breaks) %>% 
+  summarise(
+    erfasst = sum(erfasst), nicht_erfasst = sum(nicht_erfasst)
+  )
+
+zlg_28_breaks_3min <- mutate(zlg_28, breaks = cut(zlg_28$time, 
+                                                  breaks = "3 min"))
+zlg_28_grouped_3min <- zlg_28_breaks_3min %>% group_by(breaks) %>% 
+  summarise(
+    erfasst = sum(erfasst), nicht_erfasst = sum(nicht_erfasst)
+  )
+
+zlg_28_01_breaks_3min <- mutate(zlg_28_01, breaks = cut(zlg_28_01$time, 
+                                                        breaks = "3 min"))
+zlg_28_01_grouped_3min <- zlg_28_01_breaks_3min %>% group_by(breaks) %>% 
+  summarise(
+    erfasst = sum(erfasst), nicht_erfasst = sum(nicht_erfasst)
+  )
+
+zlg_28_02_breaks_3min <- mutate(zlg_28_02, breaks = cut(zlg_28_02$time, 
+                                                        breaks = "3 min"))
+zlg_28_02_grouped_3min <- zlg_28_02_breaks_3min %>% group_by(breaks) %>% 
+  summarise(
+    erfasst = sum(erfasst), nicht_erfasst = sum(nicht_erfasst)
+  )
+
+
+zlg_beide_breaks_3min <- mutate(zlg_beide, breaks = cut(zlg_beide$time, 
+                                                        breaks = "3 min"))
+zlg_beide_grouped_3min <- zlg_beide_breaks_3min %>% group_by(breaks) %>% 
+  summarise(
+    erfasst = sum(erfasst), nicht_erfasst = sum(nicht_erfasst)
+  )
+
+
+zlg_27_grouped_3min_erf <- gather(zlg_27_grouped_3min, 
+                                  key = "erfassung", value = "sum", 
+                                  erfasst, nicht_erfasst)
+
+zlg_28_01_grouped_3min_erf <- gather(zlg_28_01_grouped_3min, 
+                                     key = "erfassung", value = "sum", 
+                                     erfasst, nicht_erfasst)
+
+zlg_28_02_grouped_3min_erf <- gather(zlg_28_02_grouped_3min, 
+                                     key = "erfassung", value = "sum", 
+                                     erfasst, nicht_erfasst)
+
+
+
+
+
+
+## alle manuell eingetragenen Überschätzungswerte aus den Studentendaten raus:
+
+# neuer, bereinigter Datensatz
+
+zlg_beide_bereinigt <- zlg_beide
+
+zlg_ckpt_beide_bereinigt <- zlg_ckpt_beide
+
+
+# spielende Kinder aus manueller Zählung entfernen
+# Zeilen mit Kinder auswählen und Werte auf Null setzen
+
+zlg_beide_bereinigt[135:140, -c(1,11)] <- 0
+
+zlg_ckpt_beide_bereinigt[c(33,34), -c(1,3)] <- 0
+
+# Hund entfernen
+
+zlg_beide_bereinigt[213, -c(1,11)] <- 0
+
+zlg_ckpt_beide_bereinigt[61, -c(1,3)] <- 0
+
+# "Gerät erkennt erst "LVS erst nicht erkannt, dann doch"
+# -> passt nicht zu den Daten vom Checkpoint
+
+
+# Gerät piepst drei mal bei zwei Personen
+
+zlg_beide_bereinigt[448, -c(1,11)] <- 0
+
+zlg_ckpt_beide_bereinigt[84, -c(1,3)] <- 2
+
+
+# Gerät piepst durchgängig, da eine Person sich Schuhe auszieht
+
+zlg_beide_bereinigt[482:484, -c(1,11)] <- 0
+
+zlg_ckpt_beide_bereinigt[94:97, -c(1,3)] <- 0
+
+# Gerät piepst zwei mal, Person macht vor Gerät Foto
+
+zlg_beide_bereinigt[488, -c(1,11)] <- 0
+
+zlg_ckpt_beide_bereinigt[98, -c(1,3)] <- 0
+
+
+
+## Über gesamten Zeitraum, bereinigter Datensatz: 
+
+# Passenden Dataframe mit Summen für Plot erstellen
+
+zlg_beide_bereinigt_summen <- 
+  data.frame(type = colnames(zlg_beide_bereinigt)[-c(1,11)], 
+             sum = apply(zlg_beide_bereinigt[,-c(1,11)], 2, sum), 
+             row.names = NULL)
+zlg_beide_bereinigt_summen$type <- as.character(zlg_beide_bereinigt_summen$type)
+
+zlg_beide_bereinigt_summen <- rbind(zlg_beide_bereinigt_summen, 
+          c("checkpoint", as.numeric(sum(zlg_ckpt_beide_bereinigt$erfasst))))
+
+zlg_beide_bereinigt_summen$type <- as.factor(zlg_beide_bereinigt_summen$type)
+zlg_beide_bereinigt_summen$sum <- as.numeric(zlg_beide_bereinigt_summen$sum)
+
+zlg_beide_bereinigt_summen
+
+
+
+# Erfassung je Gruppe berechnen (für kompletten Datensatz)
+
+erfasst_je_gruppe <- zlg_beide %>% group_by(gesamt) %>% 
+  summarise(anzahl = sum(erfasst), 
+            mittel = mean(erfasst),
+            häufigkeit = n()) %>% 
+  mutate(type = "erfasst")
+
+nicht_erfasst_je_gruppe <- zlg_beide %>% group_by(gesamt) %>% 
+  summarise(anzahl = sum(nicht_erfasst),
+            mittel = mean(nicht_erfasst),
+            häufigkeit = n()) %>% 
+  mutate(type = "nicht_erfasst")
+
+erfassung_je_gruppe <- rbind(erfasst_je_gruppe, nicht_erfasst_je_gruppe) %>%
+  rename(grösse = gesamt)
+
+erfassung_je_gruppe
+
+
+# Dataframe mit Summen für alle Daten erstellen
+
+zlg_beide_summen <- data.frame(type = colnames(zlg_beide)[-c(1,11)], 
+                               sum = apply(zlg_beide[,-c(1,11)], 2, sum), 
+                               row.names = NULL)
+zlg_beide_summen$type <- as.character(zlg_beide_summen$type)
+
+zlg_beide_summen <- rbind(zlg_beide_summen, 
+                          c("checkpoint", as.numeric(sum(zlg_ckpt_beide$erfasst))))
+
+zlg_beide_summen$type <- as.factor(zlg_beide_summen$type)
+zlg_beide_summen$sum <- as.numeric(zlg_beide_summen$sum)
+
+zlg_beide_summen
+
+
+
 

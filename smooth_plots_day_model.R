bd9b7308fff1e336b5014e964b53c754b97351d9
leavanh <elektronische_post@gmx.de>
2020-05-23 15:03:20 +0200

    scale 0-1

--- smooth_plots_day_model.R
+++ smooth_plots_day_model.R
@@ -54,8 +54,10 @@ plots_day_model <- function(
                                 -2208996000, -2208985200, -2208974460), 
                        labels=c("04:00","07:00","10:00","13:00", "16:00", "19:00",
                                 "22:00", "01:00", "03:59")) +
+    scale_fill_gradient2(midpoint = 0.5, low = "blue", mid = "white",
+                        high = "green", limits = c(0, 1)) +
     ggtitle("Smoothfunktion für Uhrzeit und Datum") + 
-    labs(y="Datum", x="Uhrzeit", fill = NULL)
+    labs(y="Datum", x="Uhrzeit", fill = NULL) 
   
   # Nachbauen aus plot.gam-Daten
 

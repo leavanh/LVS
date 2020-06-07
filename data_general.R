61c67aaf00a433aa14b49afc2b0b15993521fee6
Lea Vanheyden <elektronische_post@gmx.de>
2020-06-07 12:42:07 +0200

    cloud_cover hinzugefügt

--- data_general.R
+++ data_general.R
@@ -1,8 +1,12 @@
 
 ## Diese Datei erzeugt data_general
 # Dem Szenario 1 (20% Unterschätzung) folgend werden Messungen generiert
-# Die Funktion nimmt als Argument den Anteil an Checkpoint-Messungen, der neu
-# generiert werden soll
+# Die Funktion nimmt als Argument den Anteil an Checkpoint-Messungen, die neu
+# generiert werden sollen
+
+# wir brauchen später cloud_cover
+
+cloud_cover <- readRDS(file = "Daten/cloud_cover.RDS")
 
 
 data_general_function <- function(Anteil_neu_messungen) {
@@ -16,8 +20,8 @@ n_messungen <- Anteil_neu_messungen * nrow(data_noNA)
 
 
 time_intervall <- interval( # alle möglichen Uhrzeiten
-  as.POSIXct("1899-12-31 04:00:00", tz = "MET"),
-  as.POSIXct("1900-01-01 03:59:59", tz = "MET")
+  as.POSIXct("1899-12-31 04:00:00", tz = "GMT"),
+  as.POSIXct("1900-01-01 03:59:59", tz = "GMT")
 )
 
 all_dates <- date_data_noNA$date # alle möglichen Tage
@@ -25,11 +29,11 @@ all_dates <- date_data_noNA$date # alle möglichen Tage
 # neuen data.frame erschaffen (am Ende Erste Zeile löschen)
 
 neue_messungen <- data.frame(id = NA, lvs = FALSE, position = NA,
-                     time = as.POSIXct("1899-12-31 00:00:00", tz = "MET"), 
-                     date = as.POSIXct("1899-12-31", tz = "MET"))
+                     time = as.POSIXct("1899-12-31 00:00:00", tz = "GMT"), 
+                     date = as.POSIXct("1899-12-31", tz = "GMT"))
 
 for(i in 1:n_messungen) { # neue Messungen generieren
-  messung_time <- as.POSIXct(sample(time_intervall, 1), tz = "MET", 
+  messung_time <- as.POSIXct(sample(time_intervall, 1), tz = "GMT", 
                              origin = "1899-12-31 04:00:00") # erzeugen
   messung_time <- as.POSIXct( # alle Sekunden auf 0
     trunc(messung_time, units = "mins"))
@@ -43,13 +47,29 @@ neue_messungen <- neue_messungen[-1,] # erste Zeile löschen
 
 ## Messungen hinzufügen
 
+# Allgemeine Variablen hinzufügen
+
 data_general <- neue_messungen %>%
-  full_join(date_data_noNA, by = "date") %>%
+  full_join(date_data_noNA, by = c("date")) %>%
   mutate(lvs_true_min = 0,
          lvs_false_min = 0,
          count_people_min = 0,
-         ratio_min = 0) %>%
-  rbind(data_noNA)
+         ratio_min = 0)
+
+# cloud_cover hinzufügen
+
+for(i in 1:nrow(data_general)) {
+  date <- data_general[[i, "date"]]
+  hour <- hour(data_general[[i, "time"]])
+  cloud_cover_i <- cloud_cover[cloud_cover$date == date & 
+                                 hour(cloud_cover$time) == hour,][1,] %>%
+    pull("cloud_cover")
+  data_general[i, "cloud_cover"] <- cloud_cover_i
+}
+
+# zu den "alten" Daten hinzufügen
+
+data_general <- rbind(data_general, data_noNA)
 
 # neue Summen berechnen
 
@@ -75,7 +95,7 @@ data_general <- select(data_general, - c("hour(time)", "minute(time)")) # unnöt
 date_data_general <- distinct(subset(data_general, 
                               select = -c(lvs, time, position, id, lvs_true_min,
                                           lvs_false_min, count_people_min,
-                                          ratio_min)))
+                                          ratio_min, cloud_cover)))
 
 min_data_general <- distinct(subset(data_general, 
                              select = -c(lvs, position, id)))
